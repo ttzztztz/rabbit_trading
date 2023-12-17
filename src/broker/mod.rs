c6cb81480ab5d382723641b5cbee9f106157d533
ttzztztz <ttzztztz@outlook.com>
2023-12-17 15:27:15 +0800

    add compile test tailoring - rs features

--- src/broker/mod.rs
+++ src/broker/mod.rs
@@ -1,4 +1,8 @@
 pub mod common;
 
+#[cfg(feature = "long_bridge")]
 pub mod long_bridge;
+#[cfg(feature = "yahoo_finance")]
 pub mod yahoo_finance;
+#[cfg(feature = "interactive_brokers")]
+pub mod interactive_brokers;
