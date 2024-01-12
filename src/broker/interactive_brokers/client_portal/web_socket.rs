34263a5be3936db9e8dff5644155eb46e1416569
ttzztztz <ttzztztz@outlook.com>
2024-01-12 21:26:07 +0800

    update ws err handling

--- src/broker/interactive_brokers/client_portal/web_socket.rs
+++ src/broker/interactive_brokers/client_portal/web_socket.rs
@@ -4,21 +4,24 @@ use futures_util::{
 };
 use serde_json::json;
 use tokio::net::TcpStream;
-use tokio_tungstenite::{
-    tungstenite::{Error, Message},
-    MaybeTlsStream, WebSocketStream,
-};
+use tokio_tungstenite::{tungstenite::Message, MaybeTlsStream, WebSocketStream};
 
 use super::{client::IBClientPortal, model::web_socket::Subscription};
+use crate::{
+    model::common::error::Error, utils::error::tokio_tungstenite_error_to_rabbit_trading_error,
+};
 
 pub type WriteWs = SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>;
 pub type ReadWs = SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>;
 
 //https://interactivebrokers.github.io/cpwebapi/websockets
-
 pub async fn listen(reader: &mut ReadWs, on_message: fn(String) -> ()) -> Result<(), Error> {
     while let Some(msg) = reader.next().await {
-        on_message(msg?.into_text()?);
+        on_message(
+            msg.map_err(tokio_tungstenite_error_to_rabbit_trading_error)?
+                .into_text()
+                .map_err(tokio_tungstenite_error_to_rabbit_trading_error)?,
+        );
     }
     Ok(())
 }
@@ -29,7 +32,10 @@ pub async fn keep_alive(mut writer: WriteWs) -> Result<(), Error> {
     let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(58));
     loop {
         interval.tick().await;
-        writer.send(Message::Text("tic".to_owned())).await?;
+        writer
+            .send(Message::Text("tic".to_owned()))
+            .await
+            .map_err(tokio_tungstenite_error_to_rabbit_trading_error)?;
     }
 }
 
@@ -39,8 +45,7 @@ impl IBClientPortal {
         format!("{protocol}://{}/v1/api/ws", self.host)
     }
 
-    fn ws_auth_msg(&self) -> String {
-        let session = todo!();
+    fn ws_auth_msg(&self, session: String) -> String {
         json!({ "session": session }).to_string()
     }
 
@@ -50,14 +55,23 @@ impl IBClientPortal {
         on_message: fn(String) -> (),
     ) -> Result<(), Error> {
         let url = self.get_ws_url();
-        let (ws_stream, _) = tokio_tungstenite::connect_async(url).await?;
+        let (ws_stream, _) = tokio_tungstenite::connect_async(url)
+            .await
+            .map_err(tokio_tungstenite_error_to_rabbit_trading_error)?;
         let (mut ws_out, mut ws_in) = ws_stream.split();
+
+        let session = self.tickle().await.unwrap().session;
         ws_out
-            .send(Message::Text(self.ws_auth_msg().to_owned()))
-            .await?;
+            .send(Message::Text(self.ws_auth_msg(session).to_owned()))
+            .await
+            .map_err(tokio_tungstenite_error_to_rabbit_trading_error)?;
+
         //tokio::time::sleep(tokio::time::Duration::from_millis(300)).await;
         for sub in subscriptions {
-            ws_out.send(Message::Text(sub.build())).await?;
+            ws_out
+                .send(Message::Text(sub.build()))
+                .await
+                .map_err(tokio_tungstenite_error_to_rabbit_trading_error)?;
         }
         tokio::try_join!(listen(&mut ws_in, on_message), keep_alive(ws_out))?;
         Ok(())
--- src/utils/error.rs
+++ src/utils/error.rs
@@ -7,3 +7,13 @@ pub fn reqwest_error_to_rabbit_trading_error(error: reqwest::Error) -> Error {
         message: format!("{}", error),
     }
 }
+
+pub fn tokio_tungstenite_error_to_rabbit_trading_error(
+    error: tokio_tungstenite::tungstenite::Error,
+) -> Error {
+    const TUNGSTENITE_ERROR_CODE: &'static str = "TUNGSTENITE_ERROR_CODE";
+    Error {
+        code: TUNGSTENITE_ERROR_CODE.to_owned(),
+        message: format!("{}", error),
+    }
+}
