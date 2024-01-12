5f29166090e74bd118318c86d35570d36ee96a1b
ttzztztz <ttzztztz@outlook.com>
2024-01-12 22:35:12 +0800

    add test for ibkr-cp endpoints

--- src/broker/interactive_brokers/client_portal/endpoints.rs
+++ src/broker/interactive_brokers/client_portal/endpoints.rs
@@ -100,28 +100,74 @@ impl IBClientPortal {
 
 #[cfg(test)]
 mod test_ib_cp_client {
+    use std::env;
+
+    use dotenv::dotenv;
+
     use crate::broker::interactive_brokers::client_portal::client::IBClientPortal;
 
+    const ENV_KEY_TEST_ACCOUNT: &'static str = "IBKR_TEST_ACCOUNT";
     const TEST_ACCOUNT: &'static str = "0";
-    const TEST_HOST: &'static str = "localhost";
+    const TEST_HOST: &'static str = "localhost:5000";
+    const CONID_QQQ: i64 = 320227571;
+
+    fn get_test_account() -> String {
+        dotenv().unwrap();
+        env::var(ENV_KEY_TEST_ACCOUNT).unwrap_or(TEST_ACCOUNT.to_owned())
+    }
 
     #[tokio::test]
     #[cfg_attr(feature = "ci", ignore)]
     async fn test_tickle() {
-        let ib_cp_client =
-            IBClientPortal::new(TEST_ACCOUNT.to_owned(), TEST_HOST.to_owned(), false);
+        let ib_cp_client = IBClientPortal::new(get_test_account(), TEST_HOST.to_owned(), false);
         let response_result = ib_cp_client.tickle().await;
         assert!(response_result.is_ok());
+        let response = response_result.unwrap();
+        assert!(response.session.len() > 0);
+        assert!(response.user_id > 0);
     }
 
     #[tokio::test]
     #[cfg_attr(feature = "ci", ignore)]
     async fn test_get_stocks_by_symbol() {
-        let ib_cp_client =
-            IBClientPortal::new(TEST_ACCOUNT.to_owned(), TEST_HOST.to_owned(), false);
+        let ib_cp_client = IBClientPortal::new(get_test_account(), TEST_HOST.to_owned(), false);
         let response_result = ib_cp_client
             .get_stocks_by_symbol(vec!["QQQ".to_owned()])
             .await;
         assert!(response_result.is_ok());
+        let response = response_result.unwrap();
+        let response_stock_contract_info_option = response.get("QQQ");
+        assert!(response_stock_contract_info_option.is_some());
+        let response_stock_contract_info = response_stock_contract_info_option.unwrap();
+        println!("{:?}", response_stock_contract_info);
+        assert!(response_stock_contract_info.len() > 0);
+        let contract_info = &response_stock_contract_info[0];
+        assert!(contract_info.contracts.len() > 0);
+        let contract = &contract_info.contracts[0];
+        assert!(contract.conid == CONID_QQQ);
+        assert!(contract_info.name.starts_with("INVESCO QQQ"));
+    }
+
+    #[tokio::test]
+    #[cfg_attr(feature = "ci", ignore)]
+    async fn test_get_contract_detail() {
+        let ib_cp_client = IBClientPortal::new(get_test_account(), TEST_HOST.to_owned(), false);
+        let response_result = ib_cp_client.get_contract_detail(CONID_QQQ).await;
+        assert!(response_result.is_ok());
+        let response = response_result.unwrap();
+        assert_eq!("QQQ", response.symbol);
+        assert!(response.valid_exchanges.len() > 0);
+    }
+
+    #[tokio::test]
+    #[cfg_attr(feature = "ci", ignore)]
+    async fn test_get_positions() {
+        let ib_cp_client = IBClientPortal::new(get_test_account(), TEST_HOST.to_owned(), false);
+        let response_result = ib_cp_client.get_positions(1).await;
+        assert!(response_result.is_ok());
+        let response = response_result.unwrap();
+        response.into_iter().for_each(|position| {
+            assert!(position.conid > 0);
+        });
     }
 }
